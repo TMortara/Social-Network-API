38dd8cd65e5b4c120b0947315c4c5f74b5c80dd0
Teresa <107971753+TMortara@users.noreply.github.com>
2023-03-12 18:10:33 -0500

    complete models

--- models/Thought.js
+++ models/Thought.js
@@ -1,3 +1,73 @@
 const { Schema, model } = require('mongoose');
+const { DateTime } = require('luxon');
+
+// Reactions subdocument schema
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: function() {
+                return DateTime.fromJSDate(this.createdAt).toLocaleString(DateTime.DATETIME_FULL);
+            },
+        },   
+    },
+    {
+        toJSON: {
+            getters: true,
+        }
+    }
+);
+
+// thoughtSchema is the parent document
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: function() {
+                return DateTime.fromJSDate(this.createdAt).toLocaleString(DateTime.DATETIME_FULL);
+            }
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        // Array that will hold all of the reactions
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
 
 module.exports = Thought;
