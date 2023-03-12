58389df5c0fa872cfb33d1b24ef40d87316b51d4
Teresa <107971753+TMortara@users.noreply.github.com>
2023-03-12 16:53:47 -0500

    create User model

--- models/User.js
+++ models/User.js
@@ -1,3 +1,48 @@
 const { Schema, model } = require('mongoose');
+const validator = require('mongoose-validator');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true, 
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            // match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'You must add a valid email address'],
+            validate: (value) => {
+                return validator.isEmail(value);
+            }
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Reaction',
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = model('User', userSchema);
 
 module.exports = User;
